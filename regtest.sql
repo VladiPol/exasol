2dd4bf071209346c46840922172060d3beda9c92
Vladimir Poliakov <VladiPol@users.noreply.github.com>
2019-03-18 16:59:08 +0100

    add exclued_column
    Add parameter to exclued some columns comma separated

--- regtest.sql
+++ regtest.sql
@@ -1,11 +1,24 @@
 OPEN SCHEMA REGTEST;
 
-CREATE OR REPLACE LUA SCRIPT "REGTEST_MAIN" (p_debug, src_schema_name,src_table_name,dst_schema_name,dst_table_name) RETURNS TABLE AS
-  
+CREATE OR REPLACE LUA SCRIPT "REGTEST_MAIN" (debug_flag,
+                                             src_schema_name,src_table_name,
+                                             dst_schema_name,dst_table_name,
+                                             all_exclude_columns) RETURNS TABLE AS
+-- ******************************************************
+-- crript generates the minus select of two tables
+-- for the regression test
+-- created by Vladimir Poliakov
+--
+-- input parameters are
+-- src_schema, src_table_name     - reference schema.table
+-- dst_schema_name,dst_table_name - remap schema.table
+-- all_exclude_columns            - the list of exclude columns comma separated
+-- ******************************************************  
+
 -- ******************************************************
 -- START OF function debug(output_string)
   function debug(output_string)
-    if p_debug == true then
+    if debug_flag == true then
       output(output_string)
     end
   end
@@ -14,15 +27,23 @@ CREATE OR REPLACE LUA SCRIPT "REGTEST_MAIN" (p_debug, src_schema_name,src_table_
 -- ******************************************************
 -- START OF function create_select(schema_name, table_name)
 -- ATTENTION: the database user has to have access to the table in EXA_ALL_COLUMNS
-/* 
-   The function generates the SELECT ... FROM schema_name.table_name query
-   INPUT:
-     schema_name,table_name - parameter to get all columns for query 
-   OUTPUT:
-     SELECT ... FROM ... schema_name.table_name 
-*/   
-  function create_select(schema_name, table_name)
-    debug('input parameters '..schema_name..'.'..table_name)
+-- 
+--   The function generates the SELECT ... FROM schema_name.table_name query
+--   INPUT:
+--     schema_name,table_name - parameter to get all columns for query 
+--   OUTPUT:
+--     SELECT ... FROM ... schema_name.table_name 
+--
+  function create_select(schema_name, table_name, exclude_columns)
+    debug('get columns for '..schema_name..'.'..table_name)
+
+    -- parse exclude_columns string
+    local parsed_exclude_columns = null
+    if exclude_columns ~= null then
+      parsed_exclude_columns = sqlparsing.tokenize(exclude_columns)
+      debug('exclude column(s) --> '..exclude_columns)
+    end
+    
     -- output('input parameters '..schema_name..'.'..table_name)
     -- get all columns for MINUS query
     local success, columns = pquery([[SELECT COLUMN_NAME FROM EXA_ALL_COLUMNS WHERE COLUMN_SCHEMA = :s AND COLUMN_TABLE = :t]], {s=schema_name,t=table_name})
@@ -39,12 +60,22 @@ CREATE OR REPLACE LUA SCRIPT "REGTEST_MAIN" (p_debug, src_schema_name,src_table_
     -- generate query
     local concat_str = 'SELECT '  
     for i=1, #columns do
-      if i == #columns then
-        concat_str = concat_str..columns[i][1]
-      else
-        concat_str = concat_str..columns[i][1]..', '
+      -- check exclude columns
+      local ifNotExcludedColumns = null
+      if parsed_exclude_columns ~= null then
+        ifNotExcludedColumns = sqlparsing.find(parsed_exclude_columns,1,true,false,sqlparsing.iswhitespaceorcomment,string.upper(columns[i][1]))
       end
-    end
+
+      if ifNotExcludedColumns == nil then
+        if i == #columns then
+          concat_str = concat_str..columns[i][1]
+        else
+          concat_str = concat_str..columns[i][1]..', '
+        end
+      end
+
+    end -- for i=1, #columns do
+
     concat_str = concat_str..' FROM '..schema_name..'.'..table_name
     debug('create_select RETURN -->; '..concat_str)
     -- output('create_select RETURN -->; '..concat_str)
@@ -57,7 +88,7 @@ CREATE OR REPLACE LUA SCRIPT "REGTEST_MAIN" (p_debug, src_schema_name,src_table_
 -- START OF create_MINUS_select(first_select, second_select)
   function create_MINUS_select(first_select, second_select)
     concat_str = first_select..'\n'..'MINUS'..'\n'..second_select..';'
-    debug('create_MINUS_select RETURN -->; '..concat_str)
+    debug('create_MINUS_select -->; '..concat_str)
     -- output('create_MINUS_select RETURN -->; '..concat_str)
     return concat_str
   end
@@ -69,16 +100,29 @@ CREATE OR REPLACE LUA SCRIPT "REGTEST_MAIN" (p_debug, src_schema_name,src_table_
 -- MAIN PART OF REGRESSION TEST FRAMEWORK
 -- ******************************************************  
 
-  local first_select  = create_select(src_schema_name,src_table_name)
-  local second_select = create_select(dst_schema_name,dst_table_name)
-  
+  local first_select  = create_select(src_schema_name,src_table_name,all_exclude_columns)
+  local second_select = create_select(dst_schema_name,dst_table_name,all_exclude_columns)
+  local REGTEST_OUTPUT = 'regtest_output varchar(4000)'
+
   -- get MINUS query for all differences
   result = { {create_MINUS_select(first_select, second_select)}, {create_MINUS_select(second_select, first_select)} }
-  return result, "regtest_output varchar(4000)"
+  return result, REGTEST_OUTPUT
 /
 
 COMMIT;
 
-EXECUTE SCRIPT REGTEST_MAIN (true, 'TEST_USER', 'DOENER_IMBISS', 'TEST_USER', 'DOENER_IMBISS_BKP');
+EXECUTE SCRIPT REGTEST_MAIN (true, 'TEST_USER', 'T_KUNDE_TEST', 'L0D_M00_SANDBOX_301875', 'T_KUNDE_TEST_BKP', 'ID,STRASSE');
 
-EXECUTE SCRIPT REGTEST_MAIN (true, 'TEST_USER', 'DOENER_IMBISS', 'TEST_USER', 'DOENER_IMBISS_BKP') WITH OUTPUT;
+EXECUTE SCRIPT REGTEST_MAIN (true, 'TEST_USER', 'T_KUNDE_TEST', 'L0D_M00_SANDBOX_301875', 'T_KUNDE_TEST_BKP', 'ID,STRASSE') with output;
+
+CREATE OR REPLACE TABLE T_KUNDE_TEST (
+    ID      DECIMAL(9,0),
+    TITEL   VARCHAR(20) UTF8,
+    VORNAME VARCHAR(50) UTF8 NOT NULL,
+    NAME    VARCHAR(50) UTF8 NOT NULL,
+    STRASSE VARCHAR(50) UTF8,
+    HAUSNR  VARCHAR(10) UTF8,
+    PLZ     VARCHAR(5)  UTF8,
+    STADT   VARCHAR(50) UTF8
+);
+                                                                                      
